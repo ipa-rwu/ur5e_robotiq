37e63cd9c9ae2dadcbc41d75508b24381f9fffec
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2021-01-14 01:20:23 +0100

    first commit

---
+++ ur5e_robotiq_webots/scripts/ur5e_gripper.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2021 Ruichao Wu
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import argparse
+import rospy
+
+from controller import Robot
+from joint_state_publisher_with_gripper import JointStatePublisher
+from trajectory_follower import TrajectoryFollower
+from rosgraph_msgs.msg import Clock
+
+from gripper_2f_driver import TrajectoryFollowerGripper
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--node-name', dest='nodeName', default='ur_driver', help='Specifies the name of the node.')
+arguments, unknown = parser.parse_known_args()
+
+rospy.init_node(arguments.nodeName, disable_signals=True)
+
+jointPrefix = rospy.get_param('prefix', '')
+gripperjointPrefix = rospy.get_param('prefix', '')
+if jointPrefix:
+    print('Setting prefix to %s' % jointPrefix)
+
+robot = Robot()
+nodeName = arguments.nodeName + '/' if arguments.nodeName != 'ur_driver' else ''
+jointStatePublisher = JointStatePublisher(robot, jointPrefix, nodeName)
+trajectoryFollower = TrajectoryFollower(robot, jointStatePublisher, jointPrefix, nodeName)
+trajectoryFollower.start()
+
+trajectoryFollowergripper = TrajectoryFollowerGripper(robot, jointStatePublisher, jointPrefix)
+trajectoryFollowergripper.start()
+
+# we want to use simulation time for ROS
+clockPublisher = rospy.Publisher('clock', Clock, queue_size=1)
+if not rospy.get_param('use_sim_time', False):
+    rospy.logwarn('use_sim_time is not set!')
+
+timestep = int(robot.getBasicTimeStep())
+
+while robot.step(timestep) != -1 and not rospy.is_shutdown():
+    jointStatePublisher.publish()
+    trajectoryFollower.update()
+    trajectoryFollowergripper.update()
+    # pulish simulation clock
+    msg = Clock()
+    time = robot.getTime()
+    msg.clock.secs = int(time)
+    # round prevents precision issues that can cause problems with ROS timers
+    msg.clock.nsecs = round(1000 * (time - msg.clock.secs)) * 1.0e+6
+    clockPublisher.publish(msg)
